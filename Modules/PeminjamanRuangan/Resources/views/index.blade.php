7e22525743158343395b1113213b542def97b4f8
DESKTOP-7696NKA\User <restirohmawati250@gmail.com>
2024-05-13 12:03:42 +0700

    Persiapan Modul dan Inisiasi Project PeminjamanRuangan

---
+++ Modules/PeminjamanRuangan/Resources/views/index.blade.php
@@ -0,0 +1,9 @@
+@extends('peminjamanruangan::layouts.master')
+
+@section('content')
+    <h1>Hello World</h1>
+
+    <p>
+        This view is loaded from module: {!! config('peminjamanruangan.name') !!}
+    </p>
+@endsection
