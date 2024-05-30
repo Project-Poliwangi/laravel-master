41224721620b52ffa57ff1a01ab14cb4fcec904b
RaufilPratama <pratamaraufil@gmail.com>
2024-05-30 10:48:31 +0700

    On branch main

---
+++ Modules/Monitoring/Resources/views/layouts/cahrd2.blade.php
@@ -0,0 +1,10 @@
+<!-- chart-->
+<div class="col-xl-6">
+    <div class="recent-report2">
+        <h3 class="title-3">Realisasi Keuangan</h3>
+        <div class="recent-report__chart">
+            <canvas id="doughutChart"></canvas>
+        </div>
+    </div>
+</div>
+<!-- end-->
--- Modules/Monitoring/Resources/views/layouts/master.blade.php
+++ Modules/Monitoring/Resources/views/layouts/master.blade.php
@@ -2,21 +2,71 @@
 <html lang="en">
 
 <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Module Monitoring</title>
+    <!-- Required meta tags-->
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="au theme template">
+    <meta name="author" content="Hau Nguyen">
+    <meta name="keywords" content="au theme template">
 
-    {{-- Laravel Mix - CSS File --}}
-    {{-- <link rel="stylesheet" href="{{ mix('css/monitoring.css') }}"> --}}
+    <!-- Title Page-->
+    <title>Dashboard 2</title>
+
+    <!-- Fontfaces CSS-->
+    <link href="{{assets('frontend/css/font-face.css" rel="stylesheet')}}" media="all">
+    <link href="{{assets('frontend/vendor/font-awesome-4.7/css/font-awesome.min.css')}}" rel="stylesheet" media="all">
+    <link href="{{assets('frontend/vendor/font-awesome-5/css/fontawesome-all.min.css')}}" rel="stylesheet" media="all">
+    <link href="{{assets('frontend/vendor/mdi-font/css/material-design-iconic-font.min.css')}}" rel="stylesheet"
+        media="all">
+
+    <!-- Bootstrap CSS-->
+    <link href="{{assets('frontend/vendor/bootstrap-4.1/bootstrap.min.css')}}" rel="stylesheet" media="all">
+
+    <!-- Vendor CSS-->
+    <link href="{{assets('frontend/vendor/animsition/animsition.min.css')}}" rel="stylesheet" media="all">
+    <link href="{{assets('frontend/vendor/bootstrap-progressbar/bootstrap-progressbar-3.3.4.min.css')}}"
+        rel="stylesheet" media="all">
+    <link href="{{assets('frontend/vendor/wow/animate.css" rel="stylesheet')}}" media="all">
+    <link href="{{assets('frontend/vendor/css-hamburgers/hamburgers.min.css')}}" rel="stylesheet" media="all">
+    <link href="{{assets('frontend/vendor/slick/slick.css" rel="stylesheet')}}" media="all">
+    <link href="{{assets('frontend/vendor/select2/select2.min.css')}}" rel="stylesheet" media="all">
+    <link href="{{assets('frontend/vendor/perfect-scrollbar/perfect-scrollbar.css')}}" rel="stylesheet" media="all">
+    <link href="{{assets('frontend/vendor/vector-map/jqvmap.min.css')}}" rel="stylesheet" media="all">
+
+    <!-- Main CSS-->
+    <link href="{{assets('frontend/css/theme.css')}}" rel="stylesheet" media="all">
 
 </head>
 
-<body>
+<body class="animsition">
+
+
     @yield('content')
 
-    {{-- Laravel Mix - JS File --}}
-    {{-- <script src="{{ mix('js/monitoring.js') }}"></script> --}}
+
+    <!-- Jquery JS-->
+    <script src="{{assets('frontend/vendor/jquery-3.2.1.min.js')}}"></script>
+    <!-- Bootstrap JS-->
+    <script src="{{assets('frontend/vendor/bootstrap-4.1/popper.min.js')}}"></script>
+    <script src="{{assets('frontend/vendor/bootstrap-4.1/bootstrap.min.js')}}"></script>
+    <!-- Vendor JS-->
+    <script src="{{assets('frontend/vendor/slick/slick.min.js')}}"></script>
+    <script src="{{assets('frontend/vendor/wow/wow.min.js')}}"></script>
+    <script src="{{assets('frontend/vendor/animsition/animsition.min.js')}}"></script>
+    <script src="{{assets('frontend/vendor/bootstrap-progressbar/bootstrap-progressbar.min.js')}}"></script>
+    <script src="{{assets('frontend/vendor/counter-up/jquery.waypoints.min.js')}}"></script>
+    <script src="{{assets('frontend/vendor/counter-up/jquery.counterup.min.js')}}"></script>
+    <script src="{{assets('frontend/vendor/circle-progress/circle-progress.min.js')}}"></script>
+    <script src="{{assets('frontend/vendor/perfect-scrollbar/perfect-scrollbar.js')}}"></script>
+    <script src="{{assets('frontend/vendor/chartjs/Chart.bundle.min.js')}}"></script>
+    <script src="{{assets('frontend/vendor/select2/select2.min.js')}}"></script>
+    <script src="{{assets('frontend/vendor/vector-map/jquery.vmap.js')}}"></script>
+    <script src="{{assets('frontend/vendor/vector-map/jquery.vmap.min.js')}}"></script>
+    <script src="{{assets('frontend/vendor/vector-map/jquery.vmap.sampledata.js')}}"></script>
+    <script src="{{assets('frontend/vendor/vector-map/jquery.vmap.world.js')}}"></script>
+
+    <!-- Main JS-->
+    <script src="{{assets('frontend/js/main.js')}}"></script>
 </body>
 
 </html>
