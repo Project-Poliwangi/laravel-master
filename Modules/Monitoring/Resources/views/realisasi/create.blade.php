01b8c5051155ad27bac3a4356bbca645d6a127dd
RaufilPratama <pratamaraufil@gmail.com>
2024-06-19 01:27:58 +0700

    On branch Raufil

--- Modules/Monitoring/Resources/views/realisasi/create.blade.php
+++ Modules/Monitoring/Resources/views/realisasi/create.blade.php
@@ -1,35 +1,37 @@
 @extends('adminlte::page')
-@section('title', 'Pegawai')
+@section('title', 'Realisasi')
 @section('content_header')
-    <h1 class="m-0 text-dark"></h1>
+<h1 class="m-0 text-dark"></h1>
 @stop
 @section('content')
 <div class="row">
     <div class="col-12">
         <div class="card">
-                    <div class="card-header">Tambah Realisasi</div>
-                    <div class="card-body">
-                        <a href="{{ url( (Request::server('HTTP_REFERER')==null?'/monitoring/realisasi':Request::server('HTTP_REFERER')) ) }}" title="Kembali"><button class="btn btn-warning btn-sm"><i class="fa fa-arrow-left" aria-hidden="true"></i> Kembali</button></a>
-                        <br />
-                        <br />
+            <div class="card-header">Tambah Realisasi</div>
+            <div class="card-body">
+                <a href="{{ url( (Request::server('HTTP_REFERER')==null?'/monitoring/realisasi':Request::server('HTTP_REFERER')) ) }}"
+                    title="Kembali"><button class="btn btn-warning btn-sm"><i class="fa fa-arrow-left"
+                            aria-hidden="true"></i> Kembali</button></a>
+                <br />
+                <br />
 
-                        @if ($errors->any())
-                            <ul class="alert alert-danger">
-                                @foreach ($errors->all() as $error)
-                                    <li>{{ $error }}</li>
-                                @endforeach
-                            </ul>
-                        @endif
+                @if ($errors->any())
+                <ul class="alert alert-danger">
+                    @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+                @endif
 
-                        <form method="POST" action="{{ url('/monitoring/realisasi') }}" accept-charset="UTF-8" class="form-horizontal" enctype="multipart/form-data">
-                            {{ csrf_field() }}
+                <form method="POST" action="{{ url('/monitoring/realisasi') }}" accept-charset="UTF-8"
+                    class="form-horizontal" enctype="multipart/form-data">
+                    {{ csrf_field() }}
 
-                            @include ('monitoring::realisasi.form')
+                    @include ('monitoring::realisasi.form', ['formMode' => 'create'])
 
-                        </form>
+                </form>
 
-                    </div>
-                
+            </div>
         </div>
     </div>
 </div>
--- Modules/Monitoring/Resources/views/realisasi/index.blade.php
+++ Modules/Monitoring/Resources/views/realisasi/index.blade.php
@@ -1,5 +1,5 @@
 @extends('adminlte::page')
-@section('title', 'Realisasi')
+@section('title', 'TambahRealisasi')
 @section('content_header')
     <h1 class="m-0 text-dark"></h1>
     <div class="row">
@@ -81,10 +81,81 @@
 @section('content')
     <div class="row">
         <div class="col-12">
+            <div class="card">
+                <div class="card-header">Data </div>
+                <div class="card-body">
+                    <a href="{{ route('realisasi.create') }}" class="btn btn-success btn-sm"
+                        title="Tambah Realisasi">
+                        <i class="fa fa-plus" aria-hidden="true"></i> Tambah
+                    </a>
 
-        @include('monitoring::include.tabel_realisasi')
-        
-    </div>
+                    <form method="GET" action="{{ url('/kepegawaian/pegawai') }}" accept-charset="UTF-8"
+                        class="form-inline my-2 my-lg-0 float-right" role="search">
+                        <div class="input-group">
+                            <input type="text" class="form-control" name="search" placeholder="Cari..."
+                                value="{{ request('search') }}">
+                            <span class="input-group-append">
+                                <button class="btn btn-secondary" type="submit">
+                                    <i class="fa fa-search"></i>
+                                </button>
+                            </span>
+                        </div>
+                    </form>
+
+                    <br />
+                    <br />
+                    <div class="table-responsive">
+                        <table class="table table-striped">
+                            <thead>
+                                <tr>
+                                    <th>#</th>
+                                    <th>kode</th>
+                                    <th>Nama Program Kerja</th>
+                                    <th>Aksi</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach ($perencanaans as $perencanaan)
+                                    <tr>
+                                        <td>{{ $loop->iteration }}</td>
+                                        <td>{{ $perencanaan->kode }}</td>
+                                        <td>{{ $perencanaan->nama }}</td>
+                                        <td>
+                                            <a href="{{ route('realisasi.sub_index', $perencanaan->id) }}"
+                                                title="View Realisasi">
+                                                <button class="btn btn-info btn-sm">
+                                                    <i class="fa fa-eye" aria-hidden="true"></i>
+                                                </button>
+                                            </a>
+                                            <a href="{{ url('/realisasi/' . $perencanaan->id . '/edit') }}"
+                                                title="Edit Realisasi">
+                                                <button class="btn btn-primary btn-sm">
+                                                <i class="fas fa-edit" arria-hidden="true"></i>
+                                                </button>
+                                            </a>
+
+                                            <form method="POST" action="{{ url('/realisasi/' . $perencanaan->id) }}"
+                                                accept-charset="UTF-8" style="display:inline">
+                                                {{ method_field('DELETE') }}
+                                                {{ csrf_field() }}
+                                                <button type="submit" class="btn btn-danger btn-sm"
+                                                    title="Delete Realisasi"
+                                                    onclick="return confirm(&quot;Confirm delete?&quot;)">
+                                                    <i class="fas fa-trash"></i>
+                                                </button>
+                                            </form>
+                                        </td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                        <div class="d-flex">
+                            {!! $perencanaans->links('pagination::bootstrap-4') !!}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 @endsection
 
