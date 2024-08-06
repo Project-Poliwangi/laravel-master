c1a7342d87dcbdc85d6631685977ed7e12e0484f
angelgraceriyadi <agraceriyadi@gmail.com>
2024-08-06 16:10:59 +0700

    fixed migration tabel pengadaan

--- Modules/Pengadaan/Database/Migrations/2024_07_03_154710_create_pengadaan_table.php
+++ Modules/Pengadaan/Database/Migrations/2024_07_03_154710_create_pengadaan_table.php
@@ -15,18 +15,19 @@ class CreatePengadaanTable extends Migration
     {
         Schema::create('pengadaan', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('subperencanaan_id');
             $table->string('nomor_surat');
-            $table->unsignedBigInteger('jenis_pengadaan_id');
             $table->decimal('total_biaya', 15, 2);
             $table->string('dokumen_kak')->nullable();
             $table->string('dokumen_hps')->nullable();
             $table->string('dokumen_stock_opname')->nullable();
             $table->string('dokumen_surat_ijin_impor')->nullable();
+            $table->string('dokumen_pemilihan_penyedia')->nullable();
             $table->unsignedBigInteger('status_id');
             $table->timestamps();
 
             $table->foreign('status_id')->references('id')->on('pengadaan_status')->onUpdate('CASCADE')->onDelete('CASCADE');
-            $table->foreign('jenis_pengadaan_id')->references('id')->on('jenis_pengadaans')->onUpdate('CASCADE')->onDelete('CASCADE');
+            $table->foreign('subperencanaan_id')->references('id')->on('sub_perencanaans')->onUpdate('CASCADE')->onDelete('CASCADE');
         });
     }
 
@@ -38,7 +39,7 @@ class CreatePengadaanTable extends Migration
     public function down()
     {
         Schema::table('pengadaan', function (Blueprint $table) {
-            $table->dropForeign(['jenis_pengadaan_id']);
+            $table->dropForeign(['subperencanaan_id']);
             $table->dropForeign(['status_id']);
             });
     }
