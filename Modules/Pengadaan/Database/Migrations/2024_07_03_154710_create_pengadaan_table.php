7cae6ebfc7f1e5d80932e75f2974ef61353473df
angelgraceriyadi <agraceriyadi@gmail.com>
2024-08-09 10:04:54 +0700

    memperbaiki migration perencanaans dan sub_perencanaans

--- Modules/Pengadaan/Database/Migrations/2024_07_03_154710_create_pengadaan_table.php
+++ Modules/Pengadaan/Database/Migrations/2024_07_03_154710_create_pengadaan_table.php
@@ -20,11 +20,9 @@ class CreatePengadaanTable extends Migration
             $table->string('dokumen_hps')->nullable();
             $table->string('dokumen_stock_opname')->nullable();
             $table->string('dokumen_surat_ijin_impor')->nullable();
-            $table->string('dokumen_pemilihan_penyedia')->nullable();
-            $table->unsignedBigInteger('status_id');
+            $table->string('dokumen_pemilihan_penyedia')->nullable(); 
             $table->timestamps();
 
-            $table->foreign('status_id')->references('id')->on('pengadaan_status')->onUpdate('CASCADE')->onDelete('CASCADE');
             $table->foreign('subperencanaan_id')->references('id')->on('sub_perencanaans')->onUpdate('CASCADE')->onDelete('CASCADE');
         });
     }
